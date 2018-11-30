f2d97b7fde48dc4c93b62939edadbdbf8371268d
Anitha Prince <anithaprince@Princes-MacBook-Pro.local>
2018-11-29 23:47:57 -0500

    comments seection completed

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -18,7 +18,7 @@ app.controller('AutismCtrl', ['$http', function($http){
       method:'GET',
       url: '/blogs'
     }).then(function(response){
-      console.log(response.data);
+    //  console.log(response.data);
       controller.blogs = response.data
     }, error=>{
             console.log(error);
@@ -33,7 +33,6 @@ app.controller('AutismCtrl', ['$http', function($http){
     if(this.image==''){
       this.image = "images/question.jpg"
     }
-    console.log(this.post_body);
     $http({
         method:'POST',
         url: '/blogs',
@@ -55,20 +54,41 @@ app.controller('AutismCtrl', ['$http', function($http){
     });
   }
 
-
   /*********    Show blog route      ********/
   this.getBlog = function(blog){
     $http({
       method:'GET',
       url: '/blogs/' + blog._id,
     }).then(function(response){
-      console.log(response.data);
+      //console.log(response.data);
       controller.blog = response.data,
       controller.includePath = './partials/blog_single.html';
     }, error=>{
             console.log(error);
     })
   };
+  /*********    Edit route      ********/
+  this.editBlog = function(blog){
+    let index = blog._id;
+    let arr = blog.comments;
+    console.log(arr);
+    arr.push(this.newComment);
+    console.log(arr);
+
+    console.log(index);
+    $http({
+      method:'PUT',
+      url: '/blogs/' + index,
+      data: {
+        comments: arr,
+      }
+    }).then(function(response){
+      controller.newComment= "";
+      controller.getBlog();
+
+    }).catch(angular.noop);
+  }
+
 this.getBlogs();
 
 }]);
